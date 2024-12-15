0f09e83669986ffe215148284ed10efe67861fa1
Veronika Kremneva <verkremneva@gmail.com>
2024-12-15 04:11:52 +0200

    event: added randomisation

--- FreeRTOS/Demo/Posix_GCC/myProject/events.c
+++ FreeRTOS/Demo/Posix_GCC/myProject/events.c
@@ -5,15 +5,11 @@ void vEventGeneratorTask(void *param) {
     int16_t sEventCode = 0;
     TickType_t xTicksToWait = 0;
     BaseType_t xStatusSend = 0;
-    srand(time(NULL)); 
+
     for ( ;; ) {
-        // TODO: randomize event codes
-//        sEventCode = rand() % 3 + 1;
-        sEventCode = 1;
+        sEventCode = rand() % 3 + 1;
         xStatusSend = xQueueSend(xQueueEvents, &sEventCode, portMAX_DELAY);
-        if (xStatusSend == pdPASS) {
-            printf("Event Generation Task: Sending event code SUCCESS: code is %d\n", sEventCode);
-        } else {
+        if (xStatusSend != pdPASS) {
             printf("Event Generation Task: Sending event code FAILURE: code is %d\n", sEventCode);
         }
 
