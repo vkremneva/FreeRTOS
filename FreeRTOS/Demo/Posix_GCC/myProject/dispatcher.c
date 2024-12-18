3d401287ff5d5ad1fea68be9eee018216d146906
Veronika Kremneva <verkremneva@gmail.com>
2024-12-18 23:51:42 +0200

    dispt: refactoring to using generic task, added logging

--- FreeRTOS/Demo/Posix_GCC/myProject/dispatcher.c
+++ FreeRTOS/Demo/Posix_GCC/myProject/dispatcher.c
@@ -1,7 +1,7 @@
 #include "dispatcher.h"
 
-void vDispatcherTask(void *param) {
-    QueueHandle_t xQueueEvents = (QueueHandle_t)param;
+void vDispatcherTask(void *pvParameters) {
+    department_t *pxDepartments = (department_t*)pvParameters;
     TickType_t xLastWakeTime;
     BaseType_t xStatusReceive = 0, xStatusSend = 0;
     int16_t sEventCode = 0;
@@ -12,31 +12,15 @@ void vDispatcherTask(void *param) {
     for ( ;; ) {
         xStatusReceive = xQueueReceive(xQueueEvents, &sEventCode, portMAX_DELAY);
 
-        if (xStatusReceive == pdPASS) {
-            printf("Dispatcher Task: Receiving the code SUCCESS: code is %d\n", sEventCode);
-
-            if (sEventCode == dispPOLICE_ID) {
-                xStatusSend = xQueueSend(xPoliceQueue, &sEventCode, portMAX_DELAY);
-            } else if (sEventCode == dispAMBULANCE_ID) {
-                xStatusSend = xQueueSend(xAmbulanceQueue, &sEventCode, portMAX_DELAY);
-            } else if (sEventCode == dispFIREFIGHTERS_ID) {
-                xStatusSend = xQueueSend(xFirefightersQueue, &sEventCode, portMAX_DELAY);
-            } else {
-                printf("Dispatcher Task: ERROR no department with code: %d\n", sEventCode);
+        if (xCheckPdPASS(xStatusReceive, "Dispatcher Task", logRECEIVE_CODE)) {
+            for (int i = 0; i < DEPARTMENTS_AMOUNT; i++) { 
+                if (sEventCode == pxDepartments[i].id) {
+                    xStatusSend = xQueueSend(pxDepartments[i].queue, &sEventCode, portMAX_DELAY);
+                    break;
+                }
             }
 
-            if (xStatusSend == pdPASS) {
-                printf ("Dispatcher Task: Sending the code SUCCESS\n");
-            } else if (xStatusSend == errQUEUE_FULL) {
-                printf("Dispatcher Task: Sending the code FAILURE: queue is already full\n");
-            } else {
-                printf("Dispatcher Task: Sending the code FAILURE: queue error\n");
-            }
-
-        } else if (xStatusReceive == errQUEUE_EMPTY) {
-            printf("Dispatcher Task: Receiving the code FAILURE: queue is empty\n");
-        } else {
-            printf("Dispatcher Task: Receiving the code FAILURE: queue error\n");
+            xCheckPdPASS(xStatusSend, "Dispatcher Task", logSEND_CODE);
         }
 
         vTaskDelayUntil( &xLastWakeTime, dispatcherDELAY );
--- FreeRTOS/Demo/Posix_GCC/myProject/dispatcher.h
+++ FreeRTOS/Demo/Posix_GCC/myProject/dispatcher.h
@@ -7,18 +7,15 @@
 #include "semphr.h"
 #include "queue.h"
 
+#include "department.h"
+#include "logging.h"
+
 #define dispatcherPRIORITY 3
 #define dispatcherDELAY ( pdMS_TO_TICKS( 1000 ))
 
-#define dispPOLICE_ID 1
-#define dispAMBULANCE_ID 2
-#define dispFIREFIGHTERS_ID 3
-#define dispCORONA_ID 4
+extern QueueHandle_t xQueueEvents;
+extern const int DEPARTMENTS_AMOUNT;
 
-extern QueueHandle_t xPoliceQueue;
-extern QueueHandle_t xAmbulanceQueue;
-extern QueueHandle_t xFirefightersQueue;
-
-void vDispatcherTask(void *param);
+void vDispatcherTask(void *pvParameters);
 
 #endif
