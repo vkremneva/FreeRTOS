509943fea164dd6768e1f6531f06d51570c78cfe
Veronika Kremneva <verkremneva@gmail.com>
2024-12-18 23:48:01 +0200

    events: refactoring magic numbers

--- FreeRTOS/Demo/Posix_GCC/myProject/events.c
+++ FreeRTOS/Demo/Posix_GCC/myProject/events.c
@@ -1,19 +1,19 @@
 #include "events.h"
 
-void vEventGeneratorTask(void *param) {
-    QueueHandle_t xQueueEvents = (QueueHandle_t)param;
+void vEventGeneratorTask(void *pvParameters) {
+    QueueHandle_t xQueueEvents = (QueueHandle_t)pvParameters;
     int16_t sEventCode = 0;
     TickType_t xTicksToWait = 0;
     BaseType_t xStatusSend = 0;
 
     for ( ;; ) {
-        sEventCode = rand() % 3 + 1;
+        sEventCode = rand() % eventsMAX_EVENT_CODE + eventsMIN_EVENT_CODE;
         xStatusSend = xQueueSend(xQueueEvents, &sEventCode, portMAX_DELAY);
         if (xStatusSend != pdPASS) {
             printf("Event Generation Task: Sending event code FAILURE: code is %d\n", sEventCode);
         }
 
-        xTicksToWait = pdMS_TO_TICKS( rand() % 3000 + 500 );
+        xTicksToWait = pdMS_TO_TICKS( rand() % eventMAX_WAIT_TIME + eventsMIN_WAIT_TIME );
         vTaskDelay(xTicksToWait);
     }
 }
--- FreeRTOS/Demo/Posix_GCC/myProject/events.h
+++ FreeRTOS/Demo/Posix_GCC/myProject/events.h
@@ -11,8 +11,15 @@
 // TODO not so sure about 10
 #define eventsQUEUE_SIZE 10
 #define eventsQUEUE_ITEM_SIZE (sizeof(int16_t)) 
+
 #define eventsPRIORITY 3
 
-void vEventGeneratorTask(void *param);
+#define eventsMIN_WAIT_TIME 500
+#define eventMAX_WAIT_TIME 3000
+
+#define eventsMAX_EVENT_CODE 3
+#define eventsMIN_EVENT_CODE 1
+
+void vEventGeneratorTask(void *pvParameters);
 
 #endif
