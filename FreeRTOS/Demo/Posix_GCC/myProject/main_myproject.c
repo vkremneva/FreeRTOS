94170db3cb2474c97c4c5a514d8ab73affda22bf
Veronika Kremneva <verkremneva@gmail.com>
2024-12-18 23:52:51 +0200

    main-my: refactoring to using generic task for the departments

--- FreeRTOS/Demo/Posix_GCC/myProject/main_myproject.c
+++ FreeRTOS/Demo/Posix_GCC/myProject/main_myproject.c
@@ -11,29 +11,35 @@
 
 QueueHandle_t xQueueEvents;
 
-void main_myproject(void)
-{
+// because I dont know how to send this to vDispatcherTask
+const int DEPARTMENTS_AMOUNT = 4;
+
+void main_myproject(void) {
     srand(time(NULL));
 
-    xQueueEvents = xQueueCreate(eventsQUEUE_SIZE, eventsQUEUE_ITEM_SIZE);
+    xQueueEvents = xQueueCreate( eventsQUEUE_SIZE, eventsQUEUE_ITEM_SIZE );
 
-    xPoliceQueue = xQueueCreate(deptQUEUE_SIZE, deptQUEUE_ITEM_SIZE);
-    xAmbulanceQueue = xQueueCreate(deptQUEUE_SIZE, deptQUEUE_ITEM_SIZE);
-    xFirefightersQueue = xQueueCreate(deptQUEUE_SIZE, deptQUEUE_ITEM_SIZE);
-    xCoronaQueue = xQueueCreate(deptQUEUE_SIZE, deptQUEUE_ITEM_SIZE);
+    department_t xDepartments[] = {
+        {"Police", deptPOLICE_ID, deptPOLICE_PRIORITY, deptPOLICE_CARS_TOTAL, deptPOLICE_CARS_TOTAL, 0, 0, NULL, NULL},
+        {"Ambulance", deptAMBULANCE_ID, deptAMBULANCE_PRIORITY, deptAMBULANCE_CARS_TOTAL, deptAMBULANCE_CARS_TOTAL, 0, 0, NULL, NULL},
+        {"Firefighters", deptFIREFIGHTERS_ID, deptFIREFIGHTERS_PRIORITY, deptFIREFIGHTERS_CARS_TOTAL, deptFIREFIGHTERS_CARS_TOTAL, 0, 0, NULL, NULL},
+        {"Corona", deptCORONA_ID, deptCORONA_PRIORITY, deptCORONA_CARS_TOTAL, deptCORONA_CARS_TOTAL, 0, 0, NULL, NULL}
+    };
 
-    xPoliceCountingSemaphore = xSemaphoreCreateCounting( deptPOLICE_CARS_TOTAL, deptPOLICE_CARS_TOTAL );  
-    xAmbulanceCountingSemaphore = xSemaphoreCreateCounting( deptAMBULANCE_CARS_TOTAL, deptAMBULANCE_CARS_TOTAL );  
-    xFirefightersCountingSemaphore = xSemaphoreCreateCounting( deptFIREFIGHTERS_CARS_TOTAL, deptFIREFIGHTERS_CARS_TOTAL );  
-    xCoronaCountingSemaphore = xSemaphoreCreateCounting( deptCORONA_CARS_TOTAL, deptCORONA_CARS_TOTAL );  
+    for (int i = 0; i < DEPARTMENTS_AMOUNT; ++i) {
+        xDepartments[i].queue = xQueueCreate( deptQUEUE_SIZE, deptQUEUE_ITEM_SIZE );
+        xDepartments[i].countingSemaphore = xSemaphoreCreateCounting( xDepartments[i].cars_total, xDepartments[i].cars_total );
 
-    xTaskCreate(vEventGeneratorTask, "Event Generator Task", configMINIMAL_STACK_SIZE, (void *)xQueueEvents, eventsPRIORITY, NULL);
-    xTaskCreate(vDispatcherTask, "Dispatcher Task", configMINIMAL_STACK_SIZE, (void *)xQueueEvents, dispatcherPRIORITY, NULL);
+        if (xDepartments[i].queue == NULL || xDepartments[i].countingSemaphore == NULL) {
+            printf("Initializing resources for %s department FAILED.\n", xDepartments[i].name);
+            return;
+        }
 
-    xTaskCreate(vDeptPoliceTask, "Police Task", configMINIMAL_STACK_SIZE, NULL, deptPOLICE_PRIORITY, NULL);
-    xTaskCreate(vDeptAmbulanceTask, "Ambulance Task", configMINIMAL_STACK_SIZE, NULL, deptAMBULANCE_PRIORITY, NULL);
-    xTaskCreate(vDeptFirefightersTask, "Firefighters Task", configMINIMAL_STACK_SIZE, NULL, deptFIREFIGHTERS_PRIORITY, NULL);
-    xTaskCreate(vDeptCoronaTask, "Corona Task", configMINIMAL_STACK_SIZE, NULL, deptCORONA_PRIORITY, NULL);
+        xTaskCreate( vDepartmentTask, xDepartments[i].name, configMINIMAL_STACK_SIZE, &xDepartments[i], xDepartments[i].priority, NULL );
+    }
+
+    xTaskCreate( vEventGeneratorTask, "Event Generator Task", configMINIMAL_STACK_SIZE, (void *)xQueueEvents, eventsPRIORITY, NULL );
+    xTaskCreate( vDispatcherTask, "Dispatcher Task", configMINIMAL_STACK_SIZE, (void *)xDepartments, dispatcherPRIORITY, NULL );
 
     vTaskStartScheduler();
 
