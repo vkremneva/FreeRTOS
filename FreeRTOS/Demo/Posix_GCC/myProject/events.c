51ddc2f703407965581509268beb8877207b4fda
Veronika Kremneva <verkremneva@gmail.com>
2025-01-18 20:24:51 +0200

    events: added working with the utils module

--- FreeRTOS/Demo/Posix_GCC/myProject/events.c
+++ FreeRTOS/Demo/Posix_GCC/myProject/events.c
@@ -10,7 +10,7 @@ void vEventGeneratorTask(void *pvParameters) {
         sEventCode = rand() % eventsMAX_EVENT_CODE + eventsMIN_EVENT_CODE;
         xStatusSend = xQueueSend(xQueueEvents, &sEventCode, portMAX_DELAY);
 
-        xCheckPdPASS(xStatusSend, "Event Generation Task", logSEND_CODE);
+        xCheckPdPASS(xStatusSend, logEVENT_TASK_NAME, logSEND_CODE);
 
         xTicksToWait = pdMS_TO_TICKS( rand() % eventMAX_WAIT_TIME + eventsMIN_WAIT_TIME );
         vTaskDelay(xTicksToWait);
