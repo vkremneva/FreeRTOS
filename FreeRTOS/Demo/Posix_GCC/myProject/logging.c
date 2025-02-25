5aa73bc51bb8b5a82cca77f4a236bb5fd969eae0
Veronika Kremneva <verkremneva@gmail.com>
2025-02-25 21:12:21 +0200

    logging: separate module, better messaging

--- FreeRTOS/Demo/Posix_GCC/myProject/logging.c
+++ FreeRTOS/Demo/Posix_GCC/myProject/logging.c
@@ -3,11 +3,25 @@
 void logDepartmentUsage( char *pcDepartmentName, UBaseType_t ulDepartmentID, TickType_t xUseTime ) {
     char logBuffer[logBUFFER_SIZE];
 
-    snprintf(logBuffer, logBUFFER_SIZE, "%s: %s %lu. %s. %s %lu. %s.\n",
-             pcDepartmentName, logRECEIVE_CODE, ulDepartmentID, 
-             logRESOURCE_ALLOCATED,
-             logTASK_TIME, xUseTime,
-             logTASK_SUCCES);
+    snprintf(logBuffer, logBUFFER_SIZE, "%s: Receiving the event code %lu. "
+                                        "A free resource was allocated. "
+                                        "Task lasted %lu. The task was completed.",
+             pcDepartmentName, 
+             ulDepartmentID, 
+             xUseTime
+    );
 
+    printf("%s\n", logBuffer);
+}
+
+void logNoResourceAvailable( char *pcDepartmentName ) {
+    char logBuffer[logBUFFER_SIZE];
+
+    sprintf(logBuffer, logBUFFER_SIZE, "%s: No resources available. "
+                                        "The event is remaining in the queue for further handling. "
+                                        "The task failed.",
+            pcDepartmentName
+    );
+    
     printf("%s\n", logBuffer);
 }
--- FreeRTOS/Demo/Posix_GCC/myProject/logging.h
+++ FreeRTOS/Demo/Posix_GCC/myProject/logging.h
@@ -9,17 +9,7 @@
 #define logBUFFER_SIZE 256
 #define logDEPT_NAME_MAXLEN 50
 
-#define logRECEIVE_CODE "Receiving the event code"
-#define logSEND_CODE "Sending the event code"
-#define logTAKE_SEMAPHORE "Taking the counting semaphore"
-#define logGIVE_SEMAPHORE "Giving the counting semaphore"
-#define logUSE_RESOURCE "Using the resource"
-
-#define logEVENT_TASK_NAME "Event Generation Task"
-#define logRESOURCE_ALLOCATED "A free resource was allocated"
-#define logTASK_TIME "Task lasted"
-#define logTASK_SUCCES "The task was completed"
-
 void logDepartmentUsage( char *pcDepartmentName, UBaseType_t ulDepartmentID, TickType_t xUseTime );
+void logNoResourceAvailable( char *pcDepartmentName );
 
 #endif
