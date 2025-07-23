2306ff3e8eae9feff37da4dfa02480764fb34501
Veronika Kremneva <verkremneva@gmail.com>
2025-07-23 19:16:36 +0300

    disp: added functionality of lending resources to starving departments using event group bits

--- FreeRTOS/Demo/Posix_GCC/myProject/dispatcher.c
+++ FreeRTOS/Demo/Posix_GCC/myProject/dispatcher.c
@@ -4,18 +4,67 @@ void vDispatcherTask(void *pvParameters) {
     department_t *pxDepartments = (department_t*)pvParameters;
     TickType_t xLastWakeTime = 0, xTimestamp = 0;
     BaseType_t xStatusReceive = 0, xStatusSend = 0;
-    int16_t sEventCode = 0;
+
+    EventBits_t xEventGroupValue = 0;
+    const EventBits_t xBitsToWaitFor = ( 
+        deptAMBULANCE_AVAILABLE | \
+        deptPOLICE_AVAILABLE | \
+        deptFIRE_AVAILABLE | \
+        deptCORONA_AVAILABLE 
+    );
+
+    event_t xEvent = {0, false};
 
     // get the current tick count
     xLastWakeTime = xTaskGetTickCount();
+    bool free_resource_found = false;
 
     for ( ;; ) {
-        xStatusReceive = xQueueReceive(xQueueEvents, &sEventCode, portMAX_DELAY);
+        xStatusReceive = xQueueReceive(xQueueEvents, &xEvent, portMAX_DELAY);
 
         if (xStatusReceive == pdPASS) {
-            xStatusSend = xQueueSend(pxDepartments[sEventCode].queue, &sEventCode, portMAX_DELAY);
-            xTimestamp = xTaskGetTickCount();
-            addToCSVLog(&log, xTimestamp, sEventCode, "Dispatcher Task");
+            if (!xEvent.rejected) {
+                xStatusSend = xQueueSend(pxDepartments[xEvent.code].queue, &xEvent, portMAX_DELAY);
+                xTimestamp = xTaskGetTickCount();
+
+                addToCSVLog(&log_to_csv, xTimestamp, xEvent.code, "Dispatcher Task");
+            } else {
+                xEventGroupValue = xEventGroupWaitBits(
+                    xDepartmentEventGroup,
+                    xBitsToWaitFor,
+                    pdFALSE,
+                    pdFALSE,
+                    deptMAX_GROUP_WAIT_TIME 
+                );
+
+                free_resource_found = false;
+                for (int i = 0; i < xDepartmentsAmount; ++i) {
+                    if( ( xEventGroupValue & pxDepartments[xIndByPriority[i]].bits_available )) {
+                        free_resource_found = true;
+
+                        xStatusSend = xQueueSend(pxDepartments[xIndByPriority[i]].queue, &xEvent, portMAX_DELAY);
+                        xTimestamp = xTaskGetTickCount();
+                        
+                        // debug output
+                        printf("*********EVENT %d SENT TO %d\n", xEvent.code, i + 1);
+                        addToCSVLog(&log_to_csv, xTimestamp, xEvent.code, "Dispatcher Task");
+                        break;
+                    }
+                }
+
+                if (!free_resource_found) {
+                    logNoResourceAvailable("Dispatcher");
+
+                    xStatusSend = xQueueSendToFront(xQueueEvents, &xEvent, portMAX_DELAY);
+                    if (xStatusSend == errQUEUE_FULL) {
+                        logQueueSendError("xQueueEvents");
+                    }
+
+                    // debug output
+                    printf("**********NO FREE RESOURCE FOUND\n");
+                }
+
+            }
         }
 
         vTaskDelayUntil( &xLastWakeTime, dispatcherDELAY );
