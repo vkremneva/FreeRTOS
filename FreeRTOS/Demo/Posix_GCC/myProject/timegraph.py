c751549934a8aed0b58e017e09d6bbc41b133c2f
Veronika Kremneva <verkremneva@gmail.com>
2025-09-03 20:37:07 +0300

    graph: added nicer legend

--- FreeRTOS/Demo/Posix_GCC/myProject/timegraph.py
+++ FreeRTOS/Demo/Posix_GCC/myProject/timegraph.py
@@ -12,18 +12,30 @@ res_names = [item for item in df["Entity"].unique() if item.startswith("Res")]
 res_names.sort()
 y_axis_order.extend(res_names)
 
-#df["Event Code"] = df["Event Code"].astype("category")
-#fig = px.scatter(df, x="Time", y="Entity", color='Event Code', size=[0.5]*len(df))
-
+# map event codes to "code - name"
+event_labels = {
+    "1": "1 - Police",
+    "2": "2 - Ambulance",
+    "3": "3 - Firefighters",
+    "4": "4 - Corona"
+}
 df["Event Code"] = df["Event Code"].astype(str)
-fig = px.scatter(df, y="Entity", x="Time", category_orders={"Entity": y_axis_order}, color="Event Code", symbol="Event Code")
-fig.update_traces(marker_size=8)
+df["Event Name"] = df["Event Code"].map(event_labels).fillna(df["Event Code"])
 
-#res_task_df = df[df["Entity"].str.startswith("ResTask")]
-#groups = res_task_df.groupby(["Event Code", "Entity"])
-#for _, group in groups:
-#  if len(group) == 2:
-#    item1 = group.iloc[0, :]
-#    item2 = group.iloc[1, :]
-#    fig.add_trace(go.Line(x=[item1[0], item2[0]], y=[item1["Entity"]]*2))
+# explicit order for legend
+event_order = ["1 - Police", "2 - Ambulance", "3 - Firefighters", "4 - Corona"]
+
+fig = px.scatter(
+    df,
+    y="Entity",
+    x="Time",
+    category_orders={
+        "Entity": y_axis_order,
+        "Event Name": event_order
+    },
+    color="Event Name",
+    symbol="Event Name"
+)
+
+fig.update_traces(marker_size=8)
 fig.show()
